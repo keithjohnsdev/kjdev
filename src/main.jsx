6d8a9cfb0c0e4be06a4f164a1a6792b102aac4d4
Keith Johns <keithjohnsdev@gmail.com>
2023-10-06 15:28:40 -0400

    testing browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {Landing} from "./views"
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
