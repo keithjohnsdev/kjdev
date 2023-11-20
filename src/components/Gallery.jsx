c32d4c2ee2d15772c6f8aca33e461d91eed63f48
Keith Johns <keithjohnsdev@gmail.com>
2023-11-20 11:15:48 -0500

    testing image path 3

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,28 +5,28 @@ const Gallery = () => {
   return (
     <div className="gallery">
       <GalleryImage
-        src="./gallery/una.png"
+        src="./una.png"
         url="https://keithjohnsdev.github.io/una-health/"
       />
       <GalleryImage
-        src="/gallery/misfits.png"
+        src="/misfits.png"
         url="https://keithjohnsdev.github.io/misfit-labs/"
         border
       />
       <GalleryImage
-        src="/gallery/bffl.png"
+        src="/bffl.png"
         url="https://keithjohnsdev.github.io/bffl/"
       />
       <GalleryImage
-        src="/gallery/wedocare.png"
+        src="/wedocare.png"
         url="https://keithjohnsdev.github.io/we-do-care/"
       />
       <GalleryImage
-        src="/gallery/trinity.png"
+        src="/trinity.png"
         url="https://keithjohnsdev.github.io/trinity-rehab/"
       />
       <GalleryImage
-        src="/gallery/audiocastle.png"
+        src="/audiocastle.png"
         url="https://keithjohnsdev.github.io/audiocastle-studios/"
       />
     </div>
